80ae013e9fe0811faad4d52862fb8fa13b32b8c1
Christian Adamini <christian.adamini@invision.de>
2020-06-01 22:43:12 +0200

    (Re)Name things to make it easier

--- spec/coord_calculator_spec.rb
+++ spec/coord_calculator_spec.rb
@@ -6,7 +6,7 @@ require 'coin_set_generator'
 
 RSpec.describe CoordCalculator do
   before do
-    @coins = double('test', limited_set: [[1, nil], [2, nil]])
+    @coins = double('visible_coins', limited_set: [[1, nil], [2, nil]])
   end
 
   it 'builds the sum of visible coin values' do
--- start_game.rb
+++ start_game.rb
@@ -7,30 +7,27 @@ require_relative 'lib/iterator'
 
 class Game
   def self.start
-    coin_set = CoinSet.new(ARGV[0].to_i)
+    number_of_coins = ARGV[0].to_i
+    coin_set = CoinSet.new(number_of_coins)
     Iterator.new(coin_set).flip_every_nth_coin
     coin_set.coins
   end
 
-  def self.north(limited_coins)
-    coins = coins(limited_coins)
+  def self.north(coins)
+    coins = CoinRepository.new(coins)
     coins.find(:greater, first: 256)
     coins.find(:smaller, first: 18)
     coins.find(:between, first: 40, last: 131)
     puts "N 51° 27.#{CoordCalculator.build_sum(coins: coins, extra: 3)}"
   end
 
-  def self.east(limited_coins)
-    coins = coins(limited_coins)
+  def self.east(coins)
+    coins = CoinRepository.new(coins)
     coins.find(:greater, first: 256)
     coins.find(:smaller, first: 1)
     coins.find(:between, first: 121, last: 142)
     coins.find(:between, first: 199, last: 221)
     puts "E 006° 59.#{CoordCalculator.build_sum(coins: coins, extra: 2)}"
-  end
-
-  def self.coins(limited_coins)
-    CoinRepository.new(limited_coins)
   end
 end
 
