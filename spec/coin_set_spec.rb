e57af0d409bc37674c6f1404b4e28b8c09b30862
Christian Adamini <christian.adamini@invision.de>
2020-06-03 19:58:38 +0200

    Remove duplicate test

--- spec/coin_set_spec.rb
+++ spec/coin_set_spec.rb
@@ -5,8 +5,7 @@ require 'coin_set'
 
 RSpec.describe CoinSet do
   it 'is a coin generator' do
-    coinset = CoinSet.new(1)
-    expect(coinset).to be_a(CoinSet)
+    expect(CoinSet.new(1)).to be_a(CoinSet)
   end
 
   it 'generates a number of coins' do
@@ -16,11 +15,6 @@ RSpec.describe CoinSet do
 
   it 'cannot create a zero coin coinset' do
     expect{ CoinSet.new(0) }.to raise_error ArgumentError
-  end
-
-  it 'knows the coin details' do
-    coinset = CoinSet.new(2)
-    expect(coinset.coins).to eq [[nil, 1], [nil, 2]]
   end
 
   it 'creates coins' do
