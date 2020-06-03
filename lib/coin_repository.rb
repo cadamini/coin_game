ab489e7b393f48703726c45fa11e9ff2a3f6115d
Christian Adamini <christian.adamini@invision.de>
2020-06-03 20:27:09 +0200

    Remove error handling completly

--- lib/coin_repository.rb
+++ lib/coin_repository.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# TODO: Fix Metrics/CyclomaticComplexity
 class CoinRepository
   attr_reader :coin_set, :result_set
 
@@ -10,7 +9,6 @@ class CoinRepository
   end
 
   def find(operator, **coins)
-    handle_input_error(coins[:first], *coins[:last])
     coin_set.each do |coin|
       next if coin[0].nil?
 
@@ -20,32 +18,9 @@ class CoinRepository
       when :smaller
         result_set << coin if coin[0] < coins[:first]
       when :between
-        if coin[0] > coins[:first] && coin[0] < coins[:last]
-          result_set << coin
-        end
+        result_set << coin if coin[0] > coins[:first] && coin[0] < coins[:last]
       end
     end
     result_set
-  end
-
-  private
-
-  def handle_input_error(*coin_numbers)
-    if coin_numbers.size == 1
-      if coin_numbers[0] > coin_set.size
-        raise ArgumentError,
-              "Invalid value #{coin_numbers[0]} for greater or smaller, "\
-              "Maximum value #{coin_set.size}"
-      end
-    elsif coin_numbers.size == 2
-      if coin_numbers[0] > coin_set.size ||
-         coin_numbers[1] > coin_set.size
-        raise ArgumentError,
-              "Invalid value (#{coin_numbers[0]} or #{coin_numbers[1]})"\
-              "for between, Maximum value #{coin_set.size}"
-      end
-    else
-      raise ArgumentError, 'too many arguments, max 2 allowed'
-    end
   end
 end
--- spec/coin_repository_spec.rb
+++ spec/coin_repository_spec.rb
@@ -3,69 +3,33 @@
 require 'coin_repository'
 require 'coin_set'
 
-# TODO: Fix Metrics/BlockLength
-
 RSpec.describe CoinRepository do
-  let(:two_coins) { [[1, nil], [nil, 2]] }
-  let(:three_coins) { [[1, nil], [nil, 2], [3, nil]] }
-  let(:five_coins) do
+  let(:five) do
     [[1, nil], [nil, 2], [3, nil], [4, nil], [5, nil]]
   end
 
   context '#find coins successfully' do
     it 'get a coin set' do
-      expect(CoinRepository.new(three_coins).coin_set)
-        .to eq [[1, nil], [nil, 2], [3, nil]]
+      expect(CoinRepository.new(five).coin_set)
+        .to eq [[1, nil], [nil, 2], [3, nil], [4, nil], [5, nil]]
     end
 
     it 'gets coins smaller x' do
       expect(
-        CoinRepository.new(three_coins).find(:smaller, first: 2)
+        CoinRepository.new(five).find(:smaller, first: 2)
       ).to eq [[1, nil]]
     end
 
     it 'gets coins greater than x' do
       expect(
-        CoinRepository.new(three_coins).find(:greater, first: 2)
-      ).to eq [[3, nil]]
+        CoinRepository.new(five).find(:greater, first: 2)
+      ).to eq [[3, nil], [4, nil], [5, nil]]
     end
 
     it 'gets coins between x and y' do
       expect(
-        CoinRepository.new(five_coins).find(:between, first: 2, last: 5)
+        CoinRepository.new(five).find(:between, first: 2, last: 5)
       ).to eq [[3, nil], [4, nil]]
-    end
-  end
-
-  context '#raise error' do
-    it 'gets a value greater than total coins' do
-      expect do
-        CoinRepository.new(two_coins).find(:greater, first: 3)
-      end.to raise_error ArgumentError
-    end
-
-    it 'gets a value smaller than total coins' do
-      expect do
-        CoinRepository.new([]).find(:smaller, first: 1)
-      end.to raise_error ArgumentError
-    end
-
-    context '#between' do
-      before do
-        @two_coin_repo = CoinRepository.new(two_coins)
-      end
-
-      it 'gets a second value which is greater than the last index' do
-        expect do
-          @two_coin_repo.find(:between, first: 2, last: 5)
-        end.to raise_error ArgumentError
-      end
-
-      it 'gets a first value which is before the first index' do
-        expect do
-          @two_coin_repo.find(:between, first: -1, last: 5)
-        end.to raise_error ArgumentError
-      end
     end
   end
 end
